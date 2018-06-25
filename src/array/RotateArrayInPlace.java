17157b7f273f62c7cfc9687c4ed371a4e7733fdf
Viper <defender123>
2018-06-24 21:43:45 -0700

    rotate array in place

--- src/array/RotateArrayInPlace.java
+++ src/array/RotateArrayInPlace.java
@@ -2,55 +2,56 @@ package array;
 
 public class RotateArrayInPlace {
 
-    public static void main(String[] args) {
-        int[][] mat = { {1, 2, 3, 4},
-                        {2, 5, 6, 7},
-                        {8, 6, 5, 3},
-                        {9, 2, 4, 5}};
-
-        int N = mat.length;
-
-        rotateArray(mat, N);
-
-        displayMatrix(N, mat);
-    }
-
-    private static void rotateArray(int[][] mat, int N) {
-
-        for (int x = 0; x < N / 2; x++)
-        {
-            // Consider elements in group of 4 in
-            // current square
-            for (int y = x; y < N-1-x; y++)
-            {
-                // store current cell in temp variable
-                int temp = mat[x][y];
-
-                // move values from right to top
-                mat[x][y] = mat[y][N-1-x];
-
-                // move values from bottom to right
-                mat[y][N-1-x] = mat[N-1-x][N-1-y];
-
-                // move values from left to bottom
-                mat[N-1-x][N-1-y] = mat[N-1-y][x];
-
-                // assign temp to left
-                mat[N-1-y][x] = temp;
+    public void rotate(int[][] matrix) {
+        int length = matrix.length-1;
+        int j=0;
+        while(j < matrix.length/2){
+            for(int i=j; i < length-j; i++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[length-i][j];
+                matrix[length-i][j] = matrix[length-j][length-i];
+                matrix[length-j][length-i] = matrix[i][length-j];
+                matrix[i][length-j] = temp;
             }
+            j++;
         }
     }
 
-    static void displayMatrix(int N, int mat[][])
-    {
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-                System.out.print(" " + mat[i][j]);
+    public void rotateAnti(int[][] matrix) {
+        int length = matrix.length-1;
+        int j=0;
+        while(j < matrix.length/2){
+            for(int i=j; i < length-j; i++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][length-j];
+                matrix[i][length-j] = matrix[length-j][length-i];
+                matrix[length-j][length-i] = matrix[length -i][j];
+                matrix[length -i][j] = temp;
+            }
+            j++;
+        }
+    }
 
+    private void print(int arr[][]){
+        for(int i=0; i < arr.length; i++){
+            for(int j=0; j < arr.length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
             System.out.print("\n");
         }
-        System.out.print("\n");
+    }
+
+    public static void main(String args[]){
+
+        int matrix[][] = {{1,2,3,4,20},{5,6,7,8,30},{9,10,11,12,40},{13,14,15,16,50},{21,22,23,24,25}};
+        RotateArrayInPlace ti = new RotateArrayInPlace();
+        ti.print(matrix);
+        //ti.rotate(matrix);
+        System.out.println();
+        //ti.print(matrix);
+        System.out.println();
+        ti.rotateAnti(matrix);
+        ti.print(matrix);
     }
 
 
