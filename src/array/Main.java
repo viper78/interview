bf8d2d7cd59356a1996ae2ec86295f590ecfbe4e
Viper <defender123>
2018-06-24 21:14:53 -0700

    organize files

--- src/array/Main.java
+++
@@ -1,8 +0,0 @@
-package array;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/array/SecondLargest.java
+++ src/array/SecondLargest.java
@@ -2,31 +2,30 @@ package array;
 
 public class SecondLargest {
 
-    public static void main(String[] args) {
-
+    public static void main( String[] args ) {
         int arr[] = {12, 35, 1, 10, 34, 1};
-        // O(n)
-        System.out.println(secondLargest(arr));
+        int n = arr.length;
+        print2largest(arr);
     }
 
-    private static int secondLargest(int[] arr) {
+    private static void print2largest( int[] arr ) {
 
-        int max = Integer.MIN_VALUE;
+        if (arr.length < 2) return;
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if ( arr[i] > second && arr[i] != first) {
+                second = arr[i];
             }
         }
 
-        int secondmax = Integer.MIN_VALUE;
+        System.out.println(second);
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > secondmax && arr[i] < max) {
-                secondmax = arr[i];
-            }
-        }
-
-        return secondmax;
     }
 }
