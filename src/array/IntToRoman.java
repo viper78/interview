2d086ade1ad344f14d54b8314c8da3a85890c98e
Viper <defender123>
2018-06-24 21:32:30 -0700

    integer to roman and back

--- src/array/IntToRoman.java
+++ src/array/IntToRoman.java
@@ -1,32 +1,87 @@
 package array;
 
+import java.util.*;
+
 public class IntToRoman {
 
-    static int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    static String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    public static void main (String[] args) {
 
-    public static void main(String[] args) {
-        int num = 1945;
-        int num1 = 2500;
-        System.out.println(intToRoman(num));
-        System.out.println(intToRoman(num1));
+        IntToRoman mycode = new IntToRoman();
+
+
+        Map<Integer, String> map = new HashMap();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+        int num = 1984;
+
+        String roman = mycode.convert(num, map);
+
+        System.out.println(roman);
     }
 
-    private static String intToRoman(int num) {
+    public String convert(int number, Map<Integer, String> map) {
 
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < values.length;) {
+        List<Integer> keys = new ArrayList(map.keySet());
 
-            if (num >= values[i]) {
-                num = num - values[i];
-                buffer.append(romans[i]);
+        Collections.sort(keys, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return -1 * o1.compareTo(o2);
             }
-            if (num < values[i]) {
-                i++;
+        });
+
+        for (int i = 0; i < keys.size(); i++) {
+
+            while (number >= keys.get(i)) {
+                number -= keys.get(i);
+                sb.append(map.get(keys.get(i)));
+            }
+
+        }
+        return sb.toString();
+    }
+
+    private int romanToInteger(String roman) {
+        HashMap<Character,Integer> romanMap = new HashMap<Character,Integer>();
+
+        romanMap.put('I',1);
+        romanMap.put('V',5);
+        romanMap.put('X',10);
+        romanMap.put('L',50);
+        romanMap.put('C',100);
+        romanMap.put('D',500);
+        romanMap.put('M',1000);
+
+        char[] rArr = roman.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < rArr.length; i++) {
+
+            int left = romanMap.get( rArr[i] );
+            int right = 0;
+            if (i + 1 < rArr.length) {
+                right = romanMap.get( rArr[i+1] );
+            }
+
+            if (left >= right) {
+                sum += left;
+            } else {
+                sum -= left;
             }
         }
-
-        return buffer.toString();
+        return sum;
     }
+
 }
