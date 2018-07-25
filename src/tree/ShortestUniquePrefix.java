a630b9da5983f6ad0361144031e7c510ea578682
Viper <defender123>
2018-07-24 21:53:21 -0700

    shortest unique prefix

---
+++ src/tree/ShortestUniquePrefix.java
@@ -0,0 +1,62 @@
+package tree;
+
+import java.util.*;
+
+public class ShortestUniquePrefix {
+
+    public static void main(String[] args) {
+
+        ShortestUniquePrefix shortestUniquePrefix = new ShortestUniquePrefix();
+        List<String> input = Arrays.asList("SanFrancisco", "SanJose", "SanRamon");
+        List<String> prefixes = shortestUniquePrefix.prefix(input);
+
+        System.out.println(prefixes);
+    }
+
+    public List<String> prefix(List<String> a) {
+        TrieNode root = new TrieNode();
+
+        for (String s : a) {
+            insert(root, s);
+        }
+
+        List<String> prefixes = new ArrayList<>();
+        for (String s : a) {
+            prefixes.add(findPrefix(root, s));
+        }
+
+        return prefixes;
+    }
+
+    void insert(TrieNode root, String s) {
+        TrieNode node = root;
+
+        for (Character ch : s.toCharArray()) {
+            node = node.children.computeIfAbsent(ch, c -> new TrieNode());
+            node.count++;
+        }
+    }
+
+    String findPrefix(TrieNode root, String s) {
+        TrieNode node = root;
+        char[] chars = s.toCharArray();
+        int i = 0;
+
+        StringBuilder prefix = new StringBuilder();
+        while (true) {
+            char ch = chars[i++];
+
+            prefix.append(ch);
+            node = node.children.get(ch);
+
+            if (node.count == 1) {
+                return prefix.toString();
+            }
+        }
+    }
+
+    class TrieNode {
+        int count = 0;
+        Map<Character, TrieNode> children = new HashMap<>();
+    }
+}
