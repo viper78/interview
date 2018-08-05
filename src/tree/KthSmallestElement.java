9b8dfd6dcaa2e44600d7d902a3cc9f4cc4584920
Viper <defender123>
2018-08-05 12:44:10 -0700

    kth smallest

---
+++ src/tree/KthSmallestElement.java
@@ -0,0 +1,52 @@
+package tree;
+
+import java.util.Stack;
+
+import static tree.TreeTester.createMinimalBST;
+
+public class KthSmallestElement {
+
+    public static void main(String[] args) {
+        KthSmallestElement smallestElement = new KthSmallestElement();
+        Tree root = smallestElement.populate();
+        System.out.println(smallestElement.kthSmallest(root, 2));
+    }
+
+    public Tree populate() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Tree root = createMinimalBST(array);
+        return root;
+    }
+
+    class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public int kthSmallest(Tree root, int k) {
+        Stack<Tree> st = new Stack<>();
+
+        while (root != null) {
+            st.push(root);
+            root = root.left;
+        }
+
+        while (k != 0) {
+            Tree n = st.pop();
+            k--;
+            if (k == 0) return n.data;
+            Tree right = n.right;
+            while (right != null) {
+                st.push(right);
+                right = right.left;
+            }
+        }
+
+        return -1; // never hit if k is valid
+    }
+}
