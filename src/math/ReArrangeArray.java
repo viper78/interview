4090db0b059d1732e5d55d4e918d5fee79bcfce7
Viper <defender123>
2018-08-10 22:12:44 -0700

    rearrange array

---
+++ src/math/ReArrangeArray.java
@@ -0,0 +1,25 @@
+package math;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReArrangeArray {
+
+    public static void main(String[] args) {
+        int[] nums= {3, 0, 1, 2};
+        rearrange(nums, 4);
+        Arrays.stream(nums).forEach(s-> System.out.print(s + " "));
+    }
+
+    // The function to rearrange an array in-place so that arr[i]
+    // becomes arr[arr[i]].
+    public static void rearrange(int arr[], int n) {
+        // First step: Increase all values by (arr[arr[i]]%n)*n
+        for (int i = 0; i < n; i++)
+            arr[i] += (arr[arr[i]] % n) * n;
+
+        // Second Step: Divide all values by n
+        for (int i = 0; i < n; i++)
+            arr[i] /= n;
+    }
+}
