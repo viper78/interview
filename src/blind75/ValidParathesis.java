020a25b295f54374a0b04ceb872ddf67f1f6df3c
viper <rajeshs.mail@gmail.com>
2023-01-27 19:29:18 -0800

    valid paranthesis

---
+++ src/blind75/ValidParathesis.java
@@ -0,0 +1,26 @@
+package blind75;
+
+import java.util.Stack;
+
+public class ValidParathesis {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (Character c: s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
