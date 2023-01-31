4461ee10b5de471bbf82e43ddeeb6fab5e4817a7
viper <rajeshs.mail@gmail.com>
2023-01-31 15:19:12 -0800

    fixed logic

--- src/blind75/MinInRotatedSortedArray.java
+++ src/blind75/MinInRotatedSortedArray.java
@@ -14,14 +14,14 @@ public class MinInRotatedSortedArray {
         if (nums.length == 0) return -1;
         if (nums.length == 1) return nums[0];
 
-        while(left <= right) {
-            int midpoint = (left + right) / 2;
-            if (midpoint > 0 && nums[midpoint] < nums[midpoint - 1]) {
-                return nums[midpoint];
+        while (left <= right) {
+            int midpoint = (left + right)/2;
+            if (midpoint < right-1 && nums[midpoint] > nums[midpoint + 1]) {
+                return nums[midpoint + 1];
             } else if (nums[left] < nums[midpoint] && nums[midpoint] > nums[right]) {
                 left = midpoint + 1;
             } else {
-                right = midpoint + 1;
+                right = midpoint - 1;
             }
         }
         return nums[left];
