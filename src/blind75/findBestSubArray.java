54eb9cf304ce5972de44e5b024132a65ddb65e0e
viper <rajeshs.mail@gmail.com>
2023-02-01 15:56:03 -0800

    added comments

--- src/blind75/findBestSubArray.java
+++ src/blind75/findBestSubArray.java
@@ -1,6 +1,11 @@
 package blind75;
 
 public class findBestSubArray {
+
+    /*
+    * Given an integer array nums and an integer k,
+    * find the sum of the subarray with the largest sum whose length is k.
+     */
     public static void main(String[] args) {
         int[] nums = {3, -1, 4, 12, -8, 5, 6};
         int k = 4;
