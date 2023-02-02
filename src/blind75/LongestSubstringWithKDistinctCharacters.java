68d5bf51ccd76c989afc4f3127ffdfd3fec00975
viper <rajeshs.mail@gmail.com>
2023-02-02 14:40:33 -0800

    two pointer + hashtable

---
+++ src/blind75/LongestSubstringWithKDistinctCharacters.java
@@ -0,0 +1,31 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestSubstringWithKDistinctCharacters {
+
+    public static void main(String[] args) {
+        String s ="eceba";
+        int k = 2;
+
+        int longest = 0;
+        int left = 0;
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+
+        for (int right = 0; right < s.length(); right++) {
+            char curr = s.charAt(right);
+            counts.put(curr, counts.getOrDefault(curr, 0)+ 1);
+            if (counts.size() > k) {
+                counts.remove(s.charAt(left), counts.getOrDefault(s.charAt(left), 0) - 1);
+                if (counts.get(s.charAt(left)) == 0) {
+                    counts.remove(s.charAt(left));
+                }
+                left++;
+            }
+            longest = Math.max(longest, right - left + 1);
+        }
+        System.out.println(longest);
+    }
+}
