f570c733cc8ece51a2e4504d747f8c9ca790aaf1
viper <rajeshs.mail@gmail.com>
2023-02-02 18:58:33 -0800

    simpler logic

--- src/blind75/TrappedRainWater.java
+++ src/blind75/TrappedRainWater.java
@@ -5,36 +5,29 @@ import java.util.Arrays;
 public class TrappedRainWater {
 
     public static void main(String[] args) {
-        int arr[] = {3, 0, 0, 2, 0, 4};
-        int n = 6;
+//        int arr[] = {3, 0, 0, 2, 0, 4};
+        int arr[] = {1,8,6,2,5,4,8,3,7};
+        int n = 9;
 
-        System.out.println(solve(arr, n));
+
     }
 
-    public static int solve (int[] arr, int n) {
+    public int maxArea(int[] height) {
 
-        int[] left = new int[n];
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
 
-        left[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = Math.max(left[i - 1], arr[i]);
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+                maxArea = Math.max(maxArea, height[left] * (right - left));
+                left++;
+            } else {
+                maxArea = Math.max(maxArea, height[right] * (right - left));
+                right--;
+            }
         }
-
-        System.out.println("left:"+ Arrays.toString(left));
-
-        int[] right = new int[n];
-
-        right[n-1] = arr[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            right[i] = Math.max(right[i+1], arr[i]);
-        }
-
-        System.out.println("right:"+ Arrays.toString(right));
-        int totalWater = 0;
-        for (int i = 0; i < n; i++) {
-            totalWater += Math.min(left[i], right[i]) - arr[i];
-        }
-
-        return totalWater;
+        return maxArea;
     }
 }
