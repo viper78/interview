05faf581a9914f59fab1cdc7acddeb803aba1439
viper <rajeshs.mail@gmail.com>
2023-02-04 11:09:56 -0800

    improved

--- src/blind75/isValidBST.java
+++ src/blind75/isValidBST.java
@@ -3,17 +3,17 @@ package blind75;
 public class isValidBST {
     public boolean isValidBST(TreeNode root) {
 
-        return isValidBST(root, null, null);
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     public boolean isValidBST(TreeNode root, Integer min, Integer max) {
-
         if (root == null) {
             return true;
-        } else if (min != null && min > root.val || max != null && max < root.val) {
-            return false;
-        } else {
-            return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
         }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+
     }
 }
